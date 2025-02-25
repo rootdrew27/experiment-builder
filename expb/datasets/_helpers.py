800fef51745efd3e83ce1a007541ebe9788b25b7
Andrew Root <root.drew27@gmail.com>
2025-02-25 09:21:12 -0600

    Fix issue with _new_dataset for cupy ndarrays

--- expb/datasets/_helpers.py
+++ expb/datasets/_helpers.py
@@ -3,6 +3,7 @@ from tempfile import TemporaryFile
 
 import numpy as np
 from numpy.typing import NDArray
+import cupy as cp
 
 
 def _get_idx_splits(
@@ -22,13 +23,13 @@ def _get_idx_splits(
     return np.split(idxs, split_counts)[:-1]
 
 
-def _create_new_mmap_from_ndarray(arr: NDArray) -> np.memmap:
+def _create_new_mmap_from_ndarray(arr: NDArray | cp.ndarray) -> np.memmap:
     assert not isinstance(arr, np.memmap), (
         "Creating a memmap from another memmap is likely undesired. Please note that the _data attribute of Dataset is read only and as such, multiple references to it are acceptable."
     )
     file = TemporaryFile()
     mm = np.memmap(file, dtype=arr.dtype, mode="w+", shape=arr.shape)
-    mm[:] = arr[:]
+    mm[:] = arr[:] if isinstance(arr, np.ndarray) else arr[:].get() # gets arr from cupy if not an ndarray
     mm.flush()
     mm.setflags(write=False)
     return mm
