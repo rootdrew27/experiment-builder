57f2f2382b67e7ee04eba7876fec70c745e0eac0
Andrew Root <root.drew27@gmail.com>
2025-02-25 11:02:30 -0600

    Cleaning

--- expb/__init__.py
+++ expb/__init__.py
@@ -3,6 +3,12 @@ from expb.datasets.utils import download_dataset, extract_zip_dataset
 from expb.datasets.labeling_platform import LabelingPlatform
 from expb.datasets.by import By
 
-__all__ = ["build_dataset", "download_dataset", "extract_zip_dataset", "LabelingPlatform", "By"]
+__all__ = [
+    "build_dataset",
+    "download_dataset",
+    "extract_zip_dataset",
+    "LabelingPlatform",
+    "By",
+]
 
 __doc__ = f"Valid formats for build dataset include {VALID_FORMATS}"
